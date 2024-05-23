eab64eb1d2c08cc1c0c3a408934a13485dcc0148
Invincible2211 <fynn.schur@web.de>
2024-05-23 16:09:32 +0200

    add missing interface implementation

--- src/main/java/de/yggdrasil/core/extension/ServerExtension.java
+++ src/main/java/de/yggdrasil/core/extension/ServerExtension.java
@@ -2,7 +2,7 @@ package de.yggdrasil.core.extension;
 
 import java.util.List;
 
-public abstract class ServerExtension {
+public abstract class ServerExtension implements Extension{
 
     final String name;
     final List<String> dependencies;
